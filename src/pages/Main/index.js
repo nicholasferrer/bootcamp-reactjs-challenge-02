b235898d0a3d30ec2b9db3b6b6cce30bcd32acbf
Nicholas Ferrer <nicholasferrer@hotmail.com>
2019-05-02 19:44:40 -0300

    formating data

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import moment from "moment";
 
 import logo from "../../assets/logo.png";
 
@@ -17,11 +18,15 @@ export default class Main extends Component {
     e.preventDefault();
 
     try {
-      const response = await api.get(`/repos/${this.state.repositoryInput}`);
+      const { data: repository } = await api.get(
+        `/repos/${this.state.repositoryInput}`
+      );
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
 
       this.setState({
         repositoryInput: "",
-        repositories: [...this.state.repositories, response.data]
+        repositories: [...this.state.repositories, repository]
       });
     } catch (err) {
       console.log(err);
